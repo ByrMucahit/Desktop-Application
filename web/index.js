507f7c7ba4a97fe7be41ec5983d9b64555ec7eeb
ByrMucahit <m.mucahitbayar@gmail.com>
2021-02-19 22:31:18 +0300

    Designing
    Designing has been done.

--- web/index.js
+++ web/index.js
@@ -3,14 +3,9 @@ function yt(){
     eel.ytdownload(data)(setImage)
 }
 
-const checkbox = document.querySelector('.my-form input[type="checkbox"]');
-const btns = document.querySelectorAll(".my-form button");
 
-checkbox.addEventListener("change", function() {
-  const checked = this.checked;
-  for (const btn of btns) {
-    checked ? (btn.disabled = false) : (btn.disabled = true);
-  }
-});
+var dates = document.getElementById('dates');
 
-$('.datepicker').pickadate();
+function adding(){
+  console.log(dates);
+}
