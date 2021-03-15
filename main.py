5c23d0cc5ba9ae9ecab7d949b657deb05f683b95
ByrMucahit <m.mucahitbayar@gmail.com>
2021-03-15 21:08:36 +0300

    Database
    Static Variables has been done , but Dynamic variables hasn't been done yet, working on it.

--- main.py
+++ main.py
@@ -30,17 +30,22 @@ def hellopython(text):
     open_close = text['Acik_Kapali']
     weekend_Service = text['HaftasonuHizmet']
     Regions = text['Bolgeler']
-    
+    userCount = int(text['KullaniciSayisi'])
+    countWhowillCalling= int(text['AranacakKisiSayisi'])
+    regionCount = int(text['BolgeSayisi'])
+    user = text['Kullanıcılar']
+    personeWhoWillCalled = text['AranacakKisiler']
+    Region = text['Bolgeler']
     # Create DataBase And Connection
     conn = sqlite3.connect("Demo.db")
     c = conn.cursor()
    
     # QUERY TRANSACTION
-    c.execute("CREATE TABLE IF NOT EXISTS SSZ (ID  REAL ,DATE TEXT, NAME TEXT, TELEPHONE TEXT, PHONE TEXT, ADRESS TEXT, EMAIL TEXT, ADRESSEXP TEXT, HOMEFACILITYTYPE TEXT, WORKPLACEFACILITY TEXT, STOREFACILITY TEXT, PANELTYPE TEXT, CONNECTIONTYPE TEXT, SELLER TEXT, PHONELINE TEXT, GPRS TEXT, NETLINE TEXT, MONTAJ_SORUMLUSU TEXT)")
-    c.execute("INSERT INTO SSZ (ID, DATE, NAME, TELEPHONE, PHONE, ADRESS, EMAIL, ADRESSEXP, HOMEFACILITYTYPE, WORKPLACEFACILITY, STOREFACILITY, PANELTYPE, CONNECTIONTYPE, SELLER, PHONELINE, GPRS, NETLINE, MONTAJ_SORUMLUSU) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",(Number_Of_Account, Date, Name_Of_Account, Account_Telephone, Account_Phone, Account_Adress, Account_Email, Address_Explanation, Type_Of_facility_Home, Type_Of_facility_WorkPlace, Type_Of_Facility_Store, Type_Of_Panel, Type_Of_Connection, Seling_Personel, Line_Of_Phone, GPRS, Line_Of_Net, assembly_Personel))
+    c.execute("CREATE TABLE IF NOT EXISTS SSZE (ID  REAL ,DATE TEXT, NAME TEXT, TELEPHONE TEXT, PHONE TEXT, ADRESS TEXT, EMAIL TEXT, ADRESSEXP TEXT, HOMEFACILITYTYPE TEXT, WORKPLACEFACILITY TEXT, STOREFACILITY TEXT, PANELTYPE TEXT, CONNECTIONTYPE TEXT, SELLER TEXT, PHONELINE TEXT, GPRS TEXT, NETLINE TEXT, MONTAJ_SORUMLUSU TEXT, KullaniciSayisi TEXT, AranacakKisiSayisi TEXT, BolgeSayisi TEXT)")
+    c.execute("INSERT INTO SSZE (ID, DATE, NAME, TELEPHONE, PHONE, ADRESS, EMAIL, ADRESSEXP, HOMEFACILITYTYPE, WORKPLACEFACILITY, STOREFACILITY, PANELTYPE, CONNECTIONTYPE, SELLER, PHONELINE, GPRS, NETLINE, MONTAJ_SORUMLUSU,KullaniciSayisi , AranacakKisiSayisi , BolgeSayisi ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",(Number_Of_Account, Date, Name_Of_Account, Account_Telephone, Account_Phone, Account_Adress, Account_Email, Address_Explanation, Type_Of_facility_Home, Type_Of_facility_WorkPlace, Type_Of_Facility_Store, Type_Of_Panel, Type_Of_Connection, Seling_Personel, Line_Of_Phone, GPRS, Line_Of_Net, assembly_Personel, userCount, countWhowillCalling, regionCount))
     conn.commit()
 
-    cursor = c.execute("SELECT ID, DATE, NAME, TELEPHONE, PHONE, ADRESS, EMAIL, ADRESSEXP, HOMEFACILITYTYPE, WORKPLACEFACILITY, STOREFACILITY, PANELTYPE, CONNECTIONTYPE, SELLER, PHONELINE, GPRS, NETLINE, MONTAJ_SORUMLUSU  FROM SSZ")
+    cursor = c.execute("SELECT ID, DATE, NAME, TELEPHONE, PHONE, ADRESS, EMAIL, ADRESSEXP, HOMEFACILITYTYPE, WORKPLACEFACILITY, STOREFACILITY, PANELTYPE, CONNECTIONTYPE, SELLER, PHONELINE, GPRS, NETLINE, MONTAJ_SORUMLUSU, KullaniciSayisi , AranacakKisiSayisi , BolgeSayisi  FROM SSZE")
     for raw in cursor:
         print("ID --> ", raw[0])
         print("DATE -> ", raw[1])
@@ -60,6 +65,22 @@ def hellopython(text):
         print("GPRS -> ", raw[15])
         print("NetHattı -> ", raw[16])
         print("Montaj Sorumlusu -> ", raw[17])
+        print("Kullanici Sayisi -> ", raw[18])
+        print("Aranacak Kisi Sayisi -> ", raw[19])
+        print("Bölge Sayisi -> ", raw[20])
+    for i in range(1,userCount+1):
+        print("{} . name --> {}".format(i,user[str(i)+"name"]))
+        print("{} . email --> {}".format(i,user[str(i)+"email"]))
+        print("{} . password --> {}".format(i,user[str(i)+"password"]))
+    for j in range(1,countWhowillCalling+1):
+        print("{} . isim --> {}".format(j,personeWhoWillCalled[str(j)+"isim"]))
+        print("{} . sifre --> {}".format(j,personeWhoWillCalled[str(j)+"sifre"]))
+        print("{} . telefon --> {}".format(j,personeWhoWillCalled[str(j)+"telefon"]))
+        print("{} . GSM1 --> {}".format(j,personeWhoWillCalled[str(j)+"GSM1"]))
+        print("{} . GSM2 --> {}".format(j,personeWhoWillCalled[str(j)+"GSM2"]))
+        print("{} . GSM3 --> {}".format(j,personeWhoWillCalled[str(j)+"GSM3"]))
+    for t in range(1, regionCount+1):
+        print("{} . Bolge --> {}".format(t,Region[str(t)+"Region"]))
         
 
     c.close()
